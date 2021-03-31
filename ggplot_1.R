8d601adb869ce8c1f27615371a804a6d21cee4bd
Isaac <isaac.racine@gmail.com>
2021-03-30 21:47:08 -0400

    First ggplot lecture

---
+++ ggplot_1.R
@@ -0,0 +1,12 @@
+# Isaac Racine
+# GG PLot 1
+# 30 Mar 2021
+#------------------------------------------------
+
+#-------------- Histroy  ---------------
+
+#Base model uses pen and ink model, each time call to graph literally drawing onto it
+
+#ggplot uses more senseful grammar mechanic to graph
+
+#-------------- section label ---------------
